4e950af9d61e942a96c4d40df869289dd5544600
DECOY <tph01198@naver.com>
2021-08-26 23:08:58 +0900

    21-08-26

--- algo/src/exD3/Expert1217.java
+++ algo/src/exD3/Expert1217.java
@@ -1,8 +1,21 @@
 package exD3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Expert1217 {
-    public static void main(String[] args) {
-        System.out.println(power(2,5));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 10; i++) {
+            int P=Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N=Integer.parseInt(st.nextToken());
+            int M=Integer.parseInt(st.nextToken());
+
+            System.out.println("#"+P+" "+power(N,M));
+        }
     }
     static int power(int n,int m){
         if(m==1) return n;
