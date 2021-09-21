61e6963f3e661be7edbc44b2dbec504d7a7d5683
decoy <tph01198@naver.com>
2021-09-21 23:38:45 +0900

    21-09-21

--- algo/src/BOJ/Silver/BOJ1260.java
+++ algo/src/BOJ/Silver/BOJ1260.java
@@ -3,6 +3,7 @@ package BOJ.Silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -15,27 +16,46 @@ public class BOJ1260 {
     public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
-        start = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        start = Integer.parseInt(st.nextToken());
         map = new int[N + 1][N + 1];
         visit = new boolean[N+1];
         for (int i = 0; i < M; i++) {
-            int v1 = Integer.parseInt(br.readLine());
-            int v2 = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
 
             map[v1][v2]=1;
             map[v2][v1]=1;
         }
         BFS(start);
+        Arrays.fill(visit,false);
+        DFS(start);
     }
     static  void BFS(int v){
         Queue<Integer> q = new LinkedList<>();
+        int x = map.length-1;
         q.offer(v);
         visit[v]=true;
 
        while(!q.isEmpty()){
-
+           int temp = q.poll();
+           System.out.print(temp+" ");
+           for (int i = 1; i <= x; i++) {
+               if(map[temp][i]==1&&!visit[i]){
+                   q.offer(i);
+                   visit[i]=true;
+               }
+           }
        }
+    }
+    static void DFS(int v){
+        int x = map.length-1;
+        for (int i = 0; i < x; i++) {
+            if(map[x][i]==1&&!visit[i]){
+                DFS(i);
+            }
+        }
     }
 }
