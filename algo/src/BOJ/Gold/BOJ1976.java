fd958734de624e693c16f41da625c394798964df
DECOY <tph01198@naver.com>
2021-10-07 18:02:05 +0900

    21-10-07

--- algo/src/BOJ/Gold/BOJ1976.java
+++ algo/src/BOJ/Gold/BOJ1976.java
@@ -6,10 +6,16 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ1976 {
+    static int[]parents;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
+        //make
+        for (int i = 0; i < m; i++) {
+            parents[i]=i;
+        }
         //입력값 받기
         int[][] map = new int[n][n];
         for (int i = 0; i < n; i++) {
@@ -18,5 +24,12 @@ public class BOJ1976 {
                 map[i][j]=Integer.parseInt(st.nextToken());
             }
         }
+
+    }
+    static void union(int a,int b){
+
+    }
+    static int find(int a){
+        return 0;
     }
 }
