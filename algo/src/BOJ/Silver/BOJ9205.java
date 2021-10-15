8144c44b107a618fa0a05ecb46c767deac46d7b9
DECOY <tph01198@naver.com>
2021-10-15 22:31:53 +0900

    21-10-15

--- algo/src/BOJ/Silver/BOJ9205.java
+++ algo/src/BOJ/Silver/BOJ9205.java
@@ -5,30 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ9205 {
+public class BOJ9205 {//풀이중
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
             int n = Integer.parseInt(br.readLine());
-            int[] ints = new int[n + 2];
+            int[] map = new int[102];
             for (int j = 0; j < n + 2; j++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
+                int x = Integer.parseInt(st.nextToken() );
                 int y = Integer.parseInt(st.nextToken());
-                ints[j] = x + y;
-            }
-            boolean flag = true;
-            for (int j = 0; j < ints.length - 1; j++) {
-                if (Math.abs(ints[j] - ints[j + 1]) > 1000) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) {
-                System.out.println("happy");
-            } else {
-                System.out.println("sad");
+
+
             }
         }
     }
