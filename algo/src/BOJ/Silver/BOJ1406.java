203e31f1111318d4b1c17d5344842b4204f185e6
DECOY <tph01198@naver.com>
2021-10-27 16:35:50 +0900

    21-10-27

--- algo/src/BOJ/Silver/BOJ1406.java
+++ algo/src/BOJ/Silver/BOJ1406.java
@@ -4,54 +4,60 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class BOJ1406 {//풀이중
+/**
+ * 1406.에디터
+ *
+ * 참신했다.
+ */
+public class BOJ1406 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        ArrayList<Character> ch = new ArrayList<>();
-        //문자열 담기
-        for (int i = 0; i < str.length(); i++) {
-            ch.add(str.charAt(i));
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> right = new Stack<>();
+        Stack<Character> left = new Stack<>();
+
+        String start=br.readLine();
+        for (int i = 0; i < start.length(); i++) {
+            left.push(start.charAt(i));
         }
-        int commendline = Integer.parseInt(br.readLine());
-        int cuser = ch.size();
-
-        for (int i = 0; i < commendline; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String command = st.nextToken();
-            switch (command) {
-                case "L":
-
-                    if (cuser == 0) {
-                        cuser=0;
-                    } else {
-                        cuser -= 1;
-
+        //명령
+        int command = Integer.parseInt(br.readLine());
+        for (int i = 0; i < command; i++) {
+            String alpha = br.readLine();
+            switch (alpha.charAt(0)){
+                case 'L':
+                    if(!left.isEmpty()){
+                        right.push(left.pop());
                     }
                     break;
-                case "D":
-                    if(cuser == ch.size()){
-                        cuser= ch.size();
-                    }else{
-                        cuser+=1;
+                case 'D':
+                    if(!right.isEmpty()){
+                        left.push(right.pop());
                     }
                     break;
-                case "B":
-                    if(cuser == 0 ) continue;
-                    ch.remove(cuser-1);
-                    cuser--;
+                case 'B':
+                    if(!left.isEmpty()){
+                        left.pop();
+                    }
                     break;
-                case "P":
-                    char alpha = st.nextToken().charAt(0);
-                    ch.add(cuser, alpha);
-                    cuser++;
+                case 'P':
+                    StringTokenizer st = new StringTokenizer(alpha);
+                    String p=st.nextToken();
+                    String ch = st.nextToken();
+                    left.push(ch.charAt(0));
                     break;
             }
         }
-        for (Character character : ch) {
-            System.out.print(character);
-        }
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb1 = new StringBuilder();
+        left.stream().forEach(character -> sb1.append(character));
+        right.stream().forEach(character -> sb.append(character));
+        System.out.print(sb1);
+        System.out.print(sb.reverse());
+
+
+
     }
 }
