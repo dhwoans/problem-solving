8b28359583cd2eabd7f9eaf312ef7d950ec0d83e
DECOY <tph01198@naver.com>
2021-11-29 07:08:41 +0900

    21-11-29

---
+++ algo/src/BOJ/Silver/BOJ10989.java
@@ -0,0 +1,15 @@
+package BOJ.Silver;
+/**
+ * @packageName : BOJ.Silver
+ * @fileName : BOJ_10989_수 정렬하기3
+ * @date : 2021. 11. 29.
+ * @language : JAVA
+ * @classification : BFS
+ * @time_limit : 5sec
+ * @required_time : 00:40 ~ 01:22
+ * @submissions : 1
+ * @description
+ * 숫자 구하기
+ **/
+public class BOJ10989 {
+}
