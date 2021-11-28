41eee2aeea179656cffa39448e216744febb258c
DECOY <tph01198@naver.com>
2021-11-29 01:31:50 +0900

    21-11-29

--- algo/src/BOJ/Gold/BOJ2589.java
+++ algo/src/BOJ/Gold/BOJ2589.java
@@ -6,96 +6,76 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /**
- * 2589.보물섬
- * <p>
- * dfs
- */
-public class BOJ2589 {
-    static int n;
-    static int m;
+ * @packageName : BaekJoon_2020_12_22
+ * @fileName : BJ_2589_보물섬.java
+ * @date : 2020. 12. 23.
+ * @language : JAVA
+ * @classification : BFS
+ * @time_limit : 1sec
+ * @required_time : 00:40 ~ 01:22
+ * @submissions : 1
+ * @description
+ * 1. 최단 거리로 이동하는 bfs 탐색중 가장큰 값 구하기
+ */public class BOJ2589 {
     static char[][] map;
-    static boolean[][] visited;
-    static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    static ArrayList<xxyy> arr;
-    static boolean[] check;
-    static int max;
-    static int[] xy;
-
+    static int[][] visit;
+    static int[][] dir ={{-1,0},{1,0},{0,-1},{0,1}};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        map = new char[n][m];
-        visited = new boolean[n][m];
-        arr = new ArrayList<>();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        map =new char[n][m];
+        ArrayList<xxyy> arr = new ArrayList<>();
         for (int i = 0; i < map.length; i++) {
             String str = br.readLine();
             for (int j = 0; j < map[i].length; j++) {
-                map[i][j] = str.charAt(j);
-                if (map[i][j] == 'L') {
-                    arr.add(new xxyy(i, j));
+                map[i][j]=str.charAt(j);
+                if(map[i][j]=='L'){
+                    arr.add(new xxyy(i,j));
                 }
             }
         }
-        max = Integer.MIN_VALUE;
-        check = new boolean[arr.size()];
-        xy=new int[2];
+        int max =Integer.MIN_VALUE;
 
-        int[] dep = new int[2];
-        Pick(0, 0);
-        System.out.println(max);
-    }
+        for (int i = 0; i < arr.size(); i++) {
+            visit = new int[n][m];
+            Bfs(arr.get(i).x,arr.get(i).y);
 
-    private static void Pick(int cnt, int start) {
-        if (cnt == 2) {
-            bfs();
-            return;
-        }
-        for (int i = start; i < check.length; i++) {
-            if (check[i]) continue;
-            check[i] = true;
-            xy[cnt] = i;
-            Pick(cnt + 1, i + 1);
-            check[i] = false;
-
-        }
-    }
-
-    private static void bfs() {
-        Queue<int[]> que = new LinkedList<>();
-        int value=1;
-        que.add(new int[]{arr.get(xy[0]).x, arr.get(xy[0]).y,value});
-        visited[arr.get(xy[0]).x][arr.get(xy[0]).y] = true;
-        int toX = arr.get(xy[1]).x;
-        int toY = arr.get(xy[1]).y;
-        while (!que.isEmpty()) {
-            int[] z = que.poll();
-
-            if (toX == arr.get(xy[1]).x && toY == arr.get(xy[1]).y) {
-
-                max = Math.max(z[2], max);
-            }
-            for (int i = 0; i < 4; i++) {
-                int nx = z[0] + dir[i][0];
-                int ny = z[1] + dir[i][1];
-
-                //범위체크
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                if(visited[nx][ny])continue;
-                if (map[nx][ny] == 'L') {
-                    visited[nx][ny] = true;
-                    que.add(new int[]{nx, ny,z[2]+1});
+            for (int[] ints : visit) {
+                for (int x : ints) {
+                    max=Math.max(max,x);
                 }
             }
         }
+        System.out.println(max-1);
 
     }
 
+    private static void Bfs(int x,int y) {
+       Queue<int[]> que = new LinkedList<>();
+       que.add(new int[]{x,y});
+       visit[x][y]=1;
+       while (!que.isEmpty()){
+           int[] z = que.poll();
+
+           for (int i = 0; i < 4; i++) {
+               int nx = z[0]+dir[i][0];
+               int ny = z[1]+dir[i][1];
+
+               if(nx<0||ny<0||nx>=visit.length||ny>=visit[0].length)continue;
+               if(visit[nx][ny]==0&&map[nx][ny]=='L'){
+                   que.add(new int[]{nx,ny});
+                   visit[nx][ny]=visit[z[0]][z[1]]+1;
+
+               }
+           }
+
+       }
+    }
 }
-
 class xxyy {
     int x;
     int y;
