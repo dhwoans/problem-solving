ff9c59136987cc8aa100b354508e47cf76f05b71
DECOY <tph01198@naver.com>
2021-12-05 00:18:35 +0900

    21-12-05

--- algo/src/programmers/전화번호_목록.java
+++ algo/src/programmers/전화번호_목록.java
@@ -4,15 +4,18 @@ import java.util.HashMap;
 
 public class 전화번호_목록 {
     public static void main(String[] args) {
-        solution(new String[]{"119", "97674223", "1195524421"});
+        boolean answer = solution(new String[]{"123","456","789"});
+        System.out.println(answer);
     }
     public static boolean solution(String[] phone_book) {
-        HashMap<Integer, String> map = new HashMap<>();
-        int num =0;
-        for (String s : phone_book) {
-            map.put(num++,s);
-        }
         boolean answer = true;
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < phone_book.length; i++) {
+            map.put(phone_book[i],i);
+        }
+
+
+
         return answer;
     }
 }
