7d23845253cecfa8a090a490b4a79cd6cfa8081c
DECOY <tph01198@naver.com>
2021-12-07 01:34:42 +0900

    2021-12-07

--- algo/src/BOJ/Silver/BOJ2407.java
+++ algo/src/BOJ/Silver/BOJ2407.java
@@ -4,7 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/**
+ * @packageName : BOJ.Silver
+ * @fileName : BOJ_2407_조합
+ * @date : 2021. 12. 07.
+ * @language : JAVA
+ * @classification : math
+ * @time_limit : 2sec
+ * @required_time : 00:40 ~ 01:22
+ * @submissions : 1
+ * @description
+ **/
 public class BOJ2407 {//풀이중
     public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
