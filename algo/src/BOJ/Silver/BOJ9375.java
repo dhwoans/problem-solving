90955057eb91ecc9ea00b3e0671ebe40a3f5c279
DECOY <tph01198@naver.com>
2021-12-09 05:59:11 +0900

    2021-12-09

---
+++ algo/src/BOJ/Silver/BOJ9375.java
@@ -0,0 +1,29 @@
+package BOJ.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/**
+ * @packageName : BOJ.Silver
+ * @fileName : BOJ_9375_패션왕 신해빈
+ * @date : 2021. 12. 09.
+ * @language : JAVA
+ * @classification : math
+ * @time_limit : 1sec
+ * @required_time : 00:40 ~ 01:22
+ * @submissions : 1
+ * @description
+ **/
+
+public class BOJ9375 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            for (int j = 0; j < n; j++) {
+
+            }
+        }
+    }
+}
