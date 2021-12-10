171dd19c9a1700cee42cd9bf9288932f08624af5
DECOY <tph01198@naver.com>
2021-12-11 05:39:32 +0900

    2021-12-11

---
+++ algo/src/BOJ/Bronze/BOJ4806.java
@@ -0,0 +1,21 @@
+package BOJ.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ4806 {
+    public static void main(String[] args) throws IOException {
+
+        int count=0;
+
+            BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+            while(br.readLine() != null)
+                count++;
+
+
+        System.out.println(count);
+
+
+    }
+}
