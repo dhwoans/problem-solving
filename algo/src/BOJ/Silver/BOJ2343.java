e1e0ac04e373bfc874f74bf47940faaac5e6fb61
DECOY <tph01198@naver.com>
2021-12-13 23:57:25 +0900

    2021-12-13

--- algo/src/BOJ/Silver/BOJ2343.java
+++ algo/src/BOJ/Silver/BOJ2343.java
@@ -3,8 +3,7 @@ package BOJ.Silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /**
  * @packageName : BOJ.Silver
@@ -19,20 +18,61 @@ import java.util.StringTokenizer;
  **/
 public class BOJ2343 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int get =Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
+        int N = Integer.parseInt(st.nextToken());               //강의 갯수
+        int M = Integer.parseInt(st.nextToken());               //블루레이 녹화 갯수
+        int min = 0;
+        int max = 0;
+        List<Integer> que = new ArrayList<>();                 //강의의 길이를 담는 곳
+        ArrayList<Integer> bluray = null;                      //블루레이 담는 곳
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < arr.length; i++) {
-            arr[i]= Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            int temp = Integer.parseInt(st.nextToken());
+            que.add(temp);
+            max += temp;
+            min = Math.max(min,temp);
         }
-        Arrays.sort(arr);
 
-        int result = Arrays.binarySearch(arr, 15);
-        System.out.println(result);
+        //lower bound
+        while (min <= max) {
+            int mid = (min + max)/2;                          //블루레이의 최대크기
 
+            /*
+             * 블루레이 최대 크기만큼 강의를 채운다
+             * 블루레이의 최대크기를 넘으면 새로운 블루레이에 담는다
+             *
+             */
+            int len = 0;
+            Queue<Integer> copy = new LinkedList<>(que);
+            bluray = new ArrayList<>();
+            while (!copy.isEmpty()) {
+                int temp = copy.peek();
+                len += temp;
+                if (len <= mid) {
+                    copy.poll();
+                    if(copy.isEmpty()){
+                        bluray.add(len);
+                    }
+                } else {
+                    bluray.add(len-temp);
+                    len = 0;
+                }
+            }
+
+            /*
+             * 블루레이 담는 곳 사이즈가 M보다 크면
+             * mid의 크기를 늘린다.
+             *
+             */
+            if (bluray.size() <= M) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+
+        System.out.println(min);
 
     }
 }
---
+++ algo/src/BOJ/Silver/BOJ3079.java
@@ -0,0 +1,11 @@
+package BOJ.Silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ3079 {
+    public static void main(String[] args) {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
