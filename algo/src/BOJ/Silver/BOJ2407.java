666b4aacdf8aab49b44df5cc320d6a38ec73ed96
DECOY <tph01198@naver.com>
2021-12-26 06:33:28 +0900

    2021-12-26

--- algo/src/BOJ/Silver/BOJ2407.java
+++ algo/src/BOJ/Silver/BOJ2407.java
@@ -3,6 +3,7 @@ package BOJ.Silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 /**
  * @packageName : BOJ.Silver
@@ -15,13 +16,20 @@ import java.util.StringTokenizer;
  * @submissions : 1
  * @description
  **/
-public class BOJ2407 {//풀이중
+public class BOJ2407 {
     public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         long n = Integer.parseInt(st.nextToken());
         long m = Integer.parseInt(st.nextToken());
+        BigInteger sum = BigInteger.ONE;
 
-
+        for (long i = (n-m)+1; i <=n ; i++) {
+            sum=sum.multiply(new BigInteger(String.valueOf(i)));
+        }
+        for (long i = 2; i <=m ; i++) {
+            sum=sum.divide(new BigInteger(String.valueOf(i)));
+        }
+        System.out.println(sum);
     }
 }
