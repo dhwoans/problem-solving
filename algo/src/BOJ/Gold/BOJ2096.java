48ecdbde5e500b5720cf1ae588b7a3a4f1751c29
DECOY <tph01198@naver.com>
2021-12-29 01:02:37 +0900

    2021-12-29

---
+++ algo/src/BOJ/Gold/BOJ2096.java
@@ -0,0 +1,22 @@
+package BOJ.Gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @packageName : BOJ.Gold
+ * @fileName : BOJ2096
+ * @date : 2021-12-28
+ * @language : JAVA
+ * @classification :
+ * @time_limit : 2sec
+ * @required_time : 00:40 ~ 01:22
+ * @submissions : 1
+ * @description :
+ **/
+public class BOJ2096 {
+    public static void main(String[] args) {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
