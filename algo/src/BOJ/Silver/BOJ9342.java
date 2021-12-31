ad9d05d15a901f2f066db9f34ca25ce479d1c3d1
DECOY <tph01198@naver.com>
2021-12-31 20:15:39 +0900

    2021-12-31

--- algo/src/BOJ/Silver/BOJ9342.java
+++ algo/src/BOJ/Silver/BOJ9342.java
@@ -1,4 +1,4 @@
-package BOJ.Bronze;
+package BOJ.Silver;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,30 +7,30 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * @packageName : BOJ.Bronze
- * @fileName : BOJ1264
- * @date : 2021-12-30
+ * @packageName : BOJ.Silver
+ * @fileName : BOJ9342
+ * @date : 2021-12-31
  * @language : JAVA
- * @classification : regular expression
+ * @classification : regex
  * @time_limit : 2sec
  * @required_time : 00:40 ~ 01:22
  * @submissions : 1
  * @description :
  **/
-public class BOJ1264 {
+public class BOJ9342 {
     public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-
-        while(true){
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T ; i++) {
             String str = br.readLine();
-            if(str.equals("#"))break;
-            int count =0;
-            Pattern pattern = Pattern.compile("[AEIOUaeiou]");
+            Pattern pattern = Pattern.compile("(A|B|C|D|E|F)?A{1,}F{1,}C{1,}(A|B|C|D|E|F)?");
             Matcher matcher = pattern.matcher(str);
-
-            sb.append(matcher.results().count()+"\n");
+            if(matcher.matches()){
+                System.out.println("Infected!");
+            }else{
+                System.out.println("Good");
+            }
         }
-        System.out.println(sb);
+
     }
 }
