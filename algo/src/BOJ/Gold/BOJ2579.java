406bfaed1d58bfc0bcf42330baeeeba75323ed05
decoy <tph01198@naver.com>
2022-02-24 19:10:27 +0900

    2022-02-24

---
+++ algo/src/BOJ/Gold/BOJ2579.java
@@ -0,0 +1,26 @@
+package BOJ.Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2579 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int num = 0;
+        int count =0;
+        int amount = arr[num];
+        if(arr[num+1]>arr[num+2]){
+
+        }
+    }
+
+    private static void jump() {
+
+    }
+}
