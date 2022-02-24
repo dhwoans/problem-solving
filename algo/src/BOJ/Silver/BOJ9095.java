9d8b039132c5ed472f2edf5a8e6481d279a0dbd6
decoy <tph01198@naver.com>
2022-02-24 16:34:26 +0900

    2022-02-24

--- algo/src/BOJ/Silver/BOJ9095.java
+++ algo/src/BOJ/Silver/BOJ9095.java
@@ -3,34 +3,7 @@ package BOJ.Silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-/*
- 1
- 1
 
- 2
- 1 1
- 2
-
- 3
- 1 1 1
- 2 1
- 1 2
- 3
-
- 4
- 1 1 1 1
- 2 1 1
- 2 2
- 3 1
-
-
- 5
- 1 1 1 1 1
- 2 1 1 1
- 3 1 1
-
-
- */
 public class BOJ9095 {
     public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
@@ -38,9 +11,14 @@ public class BOJ9095 {
         int[] arr = new int[12];
         arr[1]=1;
         arr[2]=2;
+        arr[3]=4;
+        for (int i = 4; i < arr.length; i++) {
+            arr[i]=arr[i-1]+arr[i-2]+arr[i-3];
+        }
+
         for (int i = 0; i < T; i++) {
             int n = Integer.parseInt(br.readLine());
-
+            System.out.println(arr[n]);
         }
     }
 }
