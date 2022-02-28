aeab1a7ecffae391909fe0ac2cdcbe3284b12eb3
DECOY <tph01198@naver.com>
2022-03-01 02:17:58 +0900

    2022-03-01

---
+++ algo/src/BOJ/Gold/BOJ14002.java
@@ -0,0 +1,26 @@
+package BOJ.Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @packageName : BOJ.Gold
+ * @fileName : BOJ14002
+ * @date : 2022-02-28
+ * @language : JAVA
+ * @classification :
+ * @time_limit : 2sec
+ * @required_time : 00:40 ~ 01:22
+ * @submissions : 1
+ * @description :
+ **/
+public class BOJ14002 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+    }
+}
