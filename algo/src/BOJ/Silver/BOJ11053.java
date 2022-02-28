df0430535d42fb7ab13d0c82c028cd22e19a812a
DECOY <tph01198@naver.com>
2022-02-28 16:47:13 +0900

    2022-02-28

---
+++ algo/src/BOJ/Silver/BOJ11053.java
@@ -0,0 +1,45 @@
+package BOJ.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @packageName : BOJ.Silver
+ * @fileName : BOJ11053
+ * @date : 2022-02-28
+ * @language : JAVA
+ * @classification :
+ * @time_limit : 2sec
+ * @required_time : 00:40 ~ 01:22
+ * @submissions : 1
+ * @description :
+ **/
+public class BOJ11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            dp[i]=1;
+            for (int j = 0; j < i; j++) {
+                if(arr[j]<arr[i]){
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                }
+            }
+        }
+        int result = 1;
+        for (int i : dp) {
+            result = Math.max(result, i);
+        }
+        System.out.println(result);
+    }
+}
