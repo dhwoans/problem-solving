65133a9d5435c9cddad6e2012a1b1a3dfc10b450
decoy <tph01198@naver.com>
2022-03-01 20:33:50 +0900

    2022-03-01

---
+++ algo/src/BOJ/Gold/BOJ1167.java
@@ -0,0 +1,75 @@
+package BOJ.Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BOJ1167 {
+
+    private static ArrayList<ArrayList<node>> arr;
+    private static boolean[] visited;
+    private static int research_node;
+    private static int answer;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        arr = new ArrayList<>();
+
+
+        for (int i = 0; i < n + 1; i++) {
+            arr.add(new ArrayList<>());
+        }
+
+        //정보받기
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            ArrayList<node> info = arr.get(num);
+            boolean flag = true;
+            int to = 0;
+            int cost = 0;
+            while (true) {
+                int temp = Integer.parseInt(st.nextToken());
+                if (temp == -1) break;
+                if (flag) {
+                    to = temp;
+                    flag = false;
+                } else {
+                    cost = temp;
+                    info.add(new node(to, cost));
+                    flag = true;
+                }
+            }
+        }
+        visited = new boolean[n + 1];
+        research_node = 0;
+        answer = 0;
+        visited[1]=true;
+        dfs(1, 0);
+        visited = new boolean[n + 1];
+        answer =0;
+        visited[research_node]=true;
+        dfs(research_node,0);
+        System.out.println(answer);
+
+    }
+
+    private static void dfs(int next, int sum) {
+        for (int i = 0; i < arr.get(next).size(); i++) {
+            node temp = arr.get(next).get(i);
+            if(!visited[temp.to]){
+                visited[temp.to]=true;
+                dfs(temp.to,sum+temp.value);
+                visited[temp.to]=false;
+            }
+            if(sum>answer){
+                answer =sum;
+                research_node = next;
+
+            }
+        }
+    }
+}
--- algo/src/BOJ/Gold/BOJ1967.java
+++ algo/src/BOJ/Gold/BOJ1967.java
@@ -16,33 +16,62 @@ import java.util.StringTokenizer;
  * @required_time : 00:40 ~ 01:22
  * @submissions : 1
  * @description :
- * 플로이드 mle
  **/
 public class BOJ1967 {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        ArrayList<ArrayList<node>> arr = new ArrayList<>();
+    private static ArrayList<ArrayList<node>> arr;
+    private static int reuslt_search;
+    private static int answer;
+    private static boolean[] visited;
 
-        for (int i = 0; i < n+1; i++) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        arr = new ArrayList<>();
+
+
+        for (int i = 0; i < n + 1; i++) {
             arr.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < n+1; i++) {
+        //정보입력
+        for (int i = 0; i < n - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int len = Integer.parseInt(st.nextToken());
 
-            arr.get(from).add(new node(to,len));
-            arr.get(to).add(new node(from,len));
+            arr.get(from).add(new node(to, len));
+            arr.get(to).add(new node(from, len));
         }
 
-        bfs();
+        reuslt_search = 0;
+        answer = 0;
+        visited = new boolean[n + 1];
+        visited[1] = true;
+        dfs(1, 0);
+        answer = 0;
+        visited = new boolean[n + 1];
+        visited[reuslt_search] = true;
+        dfs(reuslt_search, 0);
+        System.out.println(answer);
     }
 
-    private static void bfs() {
+    private static void dfs(int next, int sum) {
+        for (int i = 0; i < arr.get(next).size(); i++) {
+            node temp = arr.get(next).get(i);
+            if (!visited[temp.to]) {
+                visited[temp.to] = true;
+                dfs(temp.to, sum + temp.value);
+                visited[temp.to] = false;
+            }
+        }
+        if (answer < sum) {
+            reuslt_search = next;
+            answer = sum;
+            return;
+        }
+
 
     }
 }
