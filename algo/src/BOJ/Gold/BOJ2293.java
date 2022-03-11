8c97f8ccbd758c4dc2fabec528bf962d506c69aa
decoy <tph01198@naver.com>
2022-03-11 22:34:51 +0900

    2022-03-11

--- algo/src/BOJ/Gold/BOJ2293.java
+++ algo/src/BOJ/Gold/BOJ2293.java
@@ -5,31 +5,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ2293 {//풀이중
+public class BOJ2293 {
 
     private static int k;
     private static int n;
     private static int[] coin;
-    private static int count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        count = 0;
-        coin = new int[n];
-        int[][] dp = new int[k][n];
+        coin = new int[n+1];
+        int[] dp = new int[k+1];
 
         //정보입력
-        for (int i = 0; i < coin.length; i++) {
+        for (int i = 1; i < coin.length; i++) {
             coin[i]=Integer.parseInt(br.readLine());
         }
-
-
-
-        
-        
-        
+        dp[0]=1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = coin[i]; j < dp.length; j++) {
+                dp[j]+=dp[j-coin[i]];
+            }
+        }
+        System.out.println(dp[k]);
     }
 }
--- algo/src/BOJ/Silver/BOJ1106.java
+++ algo/src/BOJ/Silver/BOJ1106.java
@@ -3,6 +3,8 @@ package BOJ.Silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 /**
@@ -16,42 +18,51 @@ import java.util.StringTokenizer;
  * @submissions : 1
  * @description :
  **/
-public class BOJ1106 {
+public class BOJ1106 {//풀이중
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int C = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        int[] cost = new int[N + 1];
-        int[] customer = new int[N + 1];
-        int[][] map = new int[N + 1][C + 1];
+        int[][] dp = new int[N][1001];
+        final int INF = 987654321;
 
-        for (int i = 0; i < N + 1; i++) {
+        int[][] info = new int[N][2];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int c = Integer.parseInt(st.nextToken());
-            int cu = Integer.parseInt(st.nextToken());
-
-            cost[i] = c;
-            customer[i] = cu;
+            info[i][0]=Integer.parseInt(st.nextToken());
+            info[i][1]=Integer.parseInt(st.nextToken());
         }
+        Collections.sort(Arrays.asList(info),(o1, o2) -> o1[1]-o2[1]);
+
+        for (int j = 0; j < dp[0].length; j++) {
+            if(j>=info[0][1]){
+                dp[0][j]= info[0][0]+dp[0][j-info[0][1]];
+            }
+            if(j%info[0][1]==0){
+                dp[0][j] = Math.min(info[0][0] * (j / info[0][1]),dp[0][j]);
+                }
+            }
 
 
-        for (int i = 1; i < map.length; i++) {
-            for (int j = 1; j < map[0].length; j++) {
-                if (customer[i] <= j) {
-                        map[i][j] = Math.max(map[i - 1][j], cost[i] + map[i][j - customer[i]]);
-                } else {
-                    map[i][j] = map[i - 1][j];
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < dp[i].length; j++) {
+                if(j>=info[i][1]){
+                    dp[i][j]= Math.min(info[i][0]+dp[i][j-info[i][1]],dp[i-1][j]);
+                }else{
+                    dp[i][j]= dp[i-1][j];
+                }
+                if(j%info[i][1]==0){
+                    dp[i][j]= Math.min(info[i][0]*(j/info[i][1]),dp[i][j]);
                 }
             }
         }
-
-        System.out.println(map[N][C]);
-        for (int[] ints : map) {
-            for (int i : ints) {
-                System.out.print(i + "\t");
-            }
-            System.out.println();
+        int answer = Integer.MAX_VALUE;
+        for (int i = C; i < dp[0].length; i++) {
+            answer = Math.min(answer, dp[N-1][i]);
         }
+        System.out.println(answer);
     }
 }
