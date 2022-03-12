ac63361d1fabbf4e23c7d92b5bb77522e2d88f21
decoy <tph01198@naver.com>
2022-03-13 03:21:31 +0900

    2022-03-12

--- algo/src/BOJ/Gold/BOJ14503.java
+++ algo/src/BOJ/Gold/BOJ14503.java
@@ -5,98 +5,123 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * 14503.로봇 청소가
- * <p>
- * r은 북쪽으로부터 떨어진 칸의 개수, c는 서쪽으로 부터 떨어진 칸의 개수
- * <p>
- * 1.현재 위치를 청소한다.
- * 2.현재 위치에서 현재 방향을 기준으로 왼쪽 방향부터 차례대로 인접한 칸을 탐색한다.
- * a.왼쪽 방향에 아직 청소하지 않은 공간이 존재한다면, 그 방향으로 회전한 다음 한 칸을 전진하고 1번부터 진행한다.
- * b.왼쪽 방향에 청소할 공간이 없다면, 그 방향으로 회전하고 2번으로 돌아간다.
- * c.네 방향 모두 청소가 이미 되어있거나 벽인 경우에는, 바라보는 방향을 유지한 채로 한 칸 후진을 하고 2번으로 돌아간다.
- * d.네 방향 모두 청소가 이미 되어있거나 벽이면서, 뒤쪽 방향이 벽이라 후진도 할 수 없는 경우에는 작동을 멈춘다.
- */
+
 public class BOJ14503 {
+    private static class xy{
+        int x;
+        int y;
+
+        private xy(int x,int y){
+            this.x= x;
+            this.y= y;
+        }
+    }
+
+    private static class robot{
+        xy pos;
+        int dir;
+        int count;
+
+        private robot(int x,int y,int dir){
+            this.pos = new xy(x,y);
+            this.dir = dir;
+            this.count =0;
+        }
+        //청소
+        private void clean(){
+            count++;
+        }
+        //탐색
+        private boolean search(int[][] map){
+
+
+            for (int i = 0; i < 4; i++) {
+                int x =0;
+                int y =0;
+                int ch=0;
+                if(dir==0){
+                    y-=1;
+                    ch =3;
+                }
+                else if(dir==1){
+                    x-=1;
+                    ch=0;
+                }
+                else if(dir==2){
+                    y+=1;
+                    ch=1;
+                }
+                else if(dir==3) {
+                    x+=1;
+                    ch=2;
+                }
+
+                if(map[pos.x+x][pos.y+y]==0) {
+                    dir = ch;
+                    pos.x += x;
+                    pos.y += y;
+                    return true;
+                }else{
+                    dir =ch;
+                }
+            }
+            // 청소 할곳 없음
+            return back(map);
+
+        }
+        //후진
+        private boolean back(int[][] map){
+            int x =0;
+            int y =0;
+
+            if(dir==0) x+=1;
+            else if(dir==1)y-=1;
+            else if(dir==2)x-=1;
+            else y+=1;
+
+            if(map[pos.x+x][pos.y+y]==1){
+                return false;
+            }
+            pos.x+=x;
+            pos.y+=y;
+            return true;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
         int[][] map = new int[n][m];
-
-        //청소기 정보
-        //0 북, 1 동, 2 남, 3 서
         st = new StringTokenizer(br.readLine());
-        int r = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
         int dir = Integer.parseInt(st.nextToken());
 
-        //맵정보
+
+        //맵 정보
         for (int i = 0; i < map.length; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < map[i].length; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j]= Integer.parseInt(st.nextToken());
             }
         }
+        robot rb = new robot(x, y, dir);
 
-        int[][] del = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}}; //0 북, 1 동, 2 남, 3 서
-
-        out:while (true) {
-            boolean flag = false;
-            // 현재 위치 청소
-            map[r][c] = 2;
-
-            //왼쪽 방향부터 차례대로 인접한 칸을 탐색
-            if (dir == 3)dir=-1;
-            for (int i = dir + 1; i < 4; i++) {
-                int nr = r + del[i][0];
-                int nc = c + del[i][1];
-
-                if (nr >= 0 && nc >= 0 && nr < map.length && nc < map.length && map[nr][nc] == 0) {
-                    r = nr;
-                    c = nc;
-                    //방향바꿈
-                    if (dir == 3) {
-                        dir = 0;
-                    }
-                    dir++;
-
-                    break;
-                } else {
-                    //방향바꿈
-                    if (dir == 3) {
-                        dir = 0;
-                    }
-                    dir++;
-                    flag=true;
-                    continue;
-                }
+        while(true){
+            //현재 위치 청소
+            if(map[rb.pos.x][rb.pos.y]==0){
+                rb.clean();
+                map[rb.pos.x][rb.pos.y]=3;
             }
-            //다 벽이거나 청소된 구역
-            if(!flag)continue ;
-            if (r > 0 && c > 0 && r < map.length-1 && c < map.length-1) {
+            //탐색
+            boolean result =rb.search(map);
 
-                if (dir == 0 || dir == 1) {
-                    dir += 2;
-                } else {
-                    dir -= 2;
-                }
-                r = r + del[dir][0];
-                c = c + del[dir][1];
-            } else {
-                break out;
+            if(!result){
+                break;
             }
 
         }
-        int count=0;
-        for (int[] ints : map) {
-            for (int i : ints) {
-                if(i==2)count++;
-            }
-        }
-        System.out.println(count);
-
+        System.out.println(rb.count);
     }
 }
--- algo/src/BOJ/Gold/BOJ3190.java
+++ algo/src/BOJ/Gold/BOJ3190.java
@@ -7,91 +7,95 @@ import java.util.*;
 
 
 public class BOJ3190 {
-    private static class xy{
+    private static class xy {
         int x;
         int y;
 
-        private xy(int x,int y){
-            this.x=x;
-            this.y=y;
+        private xy(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
-    private static class command{
+
+    private static class command {
         int second;
         char dir;
 
-        private command(int second,char dir){
+        private command(int second, char dir) {
             this.second = second;
             this.dir = dir;
         }
     }
 
     //뱀
-    private static class snake{
-        int len=1;
-        int dir =3;
+    private static class snake {
+        int len = 1;
+        int dir = 3;
         xy pos;
         List<xy> body;
 
-        private snake(){
-            this.pos = new xy(0,0);
+        private snake() {
+            this.pos = new xy(0, 0);
             this.body = new ArrayList<xy>();
-            body.add(new xy(0,0));
-        }
-        private void eat(){
-            len+=1;
+            body.add(new xy(0, 0));
         }
 
-        private void go(){
-            if(this.dir==0){
-                this.pos.x-=1;
-            }else if(this.dir==1){
-                this.pos.x+=1;
-            }else if(this.dir==2){
-                this.pos.y-=1;
-            }else if(this.dir==3){
-                this.pos.y+=1;
+        private void eat() {
+            len += 1;
+        }
+
+        private void go() {
+            if (this.dir == 0) {
+                this.pos.x -= 1;
+            } else if (this.dir == 1) {
+                this.pos.x += 1;
+            } else if (this.dir == 2) {
+                this.pos.y -= 1;
+            } else if (this.dir == 3) {
+                this.pos.y += 1;
             }
-
-
         }
+
         //방향전환
-        private void changeDir(char c){
-            if(this.dir==0){
-                if(c=='L') dir=2;
-                else dir=3;
-            }else if(dir==1){
-                if(c=='L')dir=3;
-                else dir=2;
-            }else if(dir==2){
-                if(c=='L')dir=1;
-                else dir=0;
-            }else{
-                if(c=='L')dir=0;
-                else dir=1;
+        private void changeDir(char c) {
+            if (this.dir == 0) {
+                if (c == 'L') dir = 2;
+                else dir = 3;
+            } else if (dir == 1) {
+                if (c == 'L') dir = 3;
+                else dir = 2;
+            } else if (dir == 2) {
+                if (c == 'L') dir = 1;
+                else dir = 0;
+            } else {
+                if (c == 'L') dir = 0;
+                else dir = 1;
             }
         }
+
         // 몸통 체크
-        private void bodycheck(){
-            if(body.size()!=len){
+        private void bodycheck() {
+            if (body.size() != len) {
                 body.remove(0);
             }
         }
+
         // 충돌 체크
-        private boolean crush(){
+        private boolean crush() {
             for (BOJ3190.xy z : body) {
-                if(pos.x==z.x&&pos.y==z.y){
+                if (pos.x == z.x && pos.y == z.y) {
                     return true;
                 }
 
             }
-            body.add(new xy(pos.x,pos.y));
+            body.add(new xy(pos.x, pos.y));
             return false;
         }
 
     }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int k = Integer.parseInt(br.readLine());
 
@@ -103,7 +107,7 @@ public class BOJ3190 {
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            map[x-1][y-1]=1;
+            map[x - 1][y - 1] = 1;
         }
 
         int com = Integer.parseInt(br.readLine());
@@ -114,39 +118,39 @@ public class BOJ3190 {
             int s = Integer.parseInt(st.nextToken());
             char c = st.nextToken().charAt(0);
 
-            commands.add(new command(s+1,c));
+            commands.add(new command(s + 1, c));
         }
-        int turn =1;
+        int turn = 1;
         snake s = new snake();
-        while (true){
-           //머리 다음 칸에 위치
-           s.go();
-           if(s.pos.x<0||s.pos.y<0||s.pos.x>=n||s.pos.y>=n){
-               break;
-           }
-           if(s.crush()){
-               break;
-           }
+        while (true) {
+            //머리 다음 칸에 위치
+            s.go();
+            if (s.pos.x < 0 || s.pos.y < 0 || s.pos.x >= n || s.pos.y >= n) {
+                break;
+            }
+            if (s.crush()) {
+                break;
+            }
 
-           //이동한 칸에 사과 있는지 확인
-           if(map[s.pos.x][s.pos.y]==1){
-               s.eat();
-               map[s.pos.x][s.pos.y]=0;
-           }
+            //이동한 칸에 사과 있는지 확인
+            if (map[s.pos.x][s.pos.y] == 1) {
+                s.eat();
+                map[s.pos.x][s.pos.y] = 0;
+            }
 
-           //몸통체크
-           s.bodycheck();
+            //몸통체크
+            s.bodycheck();
 
-           //턴증가
-           turn++;
+            //턴증가
+            turn++;
 
-           //방향전환
-           if(commands.isEmpty())continue;
-           if(turn==commands.peek().second){
-               s.changeDir(commands.peek().dir);
-               commands.poll();
-           }
-       }
+            //방향전환
+            if (commands.isEmpty()) continue;
+            if (turn == commands.peek().second) {
+                s.changeDir(commands.peek().dir);
+                commands.poll();
+            }
+        }
         System.out.println(turn);
     }
 }
