b3884a7fa61ac863560f8e8620dfefc132e84e15
decoy <tph01198@naver.com>
2022-03-19 23:03:14 +0900

    2022-03-19

--- algo/src/BOJ/BOJ7662.java
+++ algo/src/BOJ/BOJ7662.java
@@ -5,38 +5,47 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class BOJ7662 {//풀이중
+public class BOJ7662 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        int T =Integer.parseInt(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
-            ArrayList<Integer> arr = new ArrayList<>();
-            int command = Integer.parseInt(br.readLine());
-            for (int j = 0; j < command; j++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                char ch = st.nextToken().charAt(0);
-                int num = Integer.parseInt(st.nextToken());
-                switch (ch){
-                    case 'I':
-                        arr.add(num);
-                        arr.sort((o1, o2) -> o1-o2);
-                        break;
-                    case 'D':
-                        if(arr.isEmpty())break;
-                        if(num==-1){
-                            arr.remove(0);
-                        }else{
-                           arr.remove(arr.size()-1);
-                        }
-                        break;
-                }
+            int k = Integer.parseInt(br.readLine());
+            StringBuilder sb = new StringBuilder();
+            TreeMap<Integer,Integer> map = new TreeMap();
 
+            for (int j = 0; j < k; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                char command = st.nextToken().charAt(0);
+                int num = Integer.parseInt(st.nextToken());
+
+                if (command == 'I') {
+                    map.put(num,map.getOrDefault(num,0)+1);
+                } else if (command == 'D') {
+                    if (num == -1 &&!map.isEmpty()) {
+                        if(map.get(map.firstKey())==1){
+                            map.remove(map.firstKey());
+                        }else {
+                            map.put(map.firstKey(),map.get(map.firstKey())-1);
+                        }
+                        continue;
+                    }
+                    if (num == 1&&!map.isEmpty() ) {
+                        if(map.get(map.lastKey())==1){
+                            map.remove(map.lastKey());
+                        }else {
+                            map.put(map.lastKey(),map.get(map.lastKey())-1);
+                        }
+                        continue;
+                    }
+                }
             }
-            if(arr.isEmpty()){
-                System.out.println("EMPTY");
-            }else {
-                System.out.println(arr.get(arr.size()-1)+" "+arr.get(0));
+            if (map.isEmpty()) {
+                sb.append("EMPTY");
+            } else {
+                sb.append(map.lastKey()).append(" ").append(map.firstKey());
             }
+            System.out.println(sb);
         }
     }
 }
