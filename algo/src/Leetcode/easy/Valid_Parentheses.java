9d6b0054d5d9ef28152b721933d5627a2f5e6680
decoy <tph01198@naver.com>
2022-04-05 01:32:32 +0900

    2022-04-05

--- algo/src/Leetcode/easy/Valid_Parentheses.java
+++ algo/src/Leetcode/easy/Valid_Parentheses.java
@@ -3,7 +3,24 @@ package Leetcode.easy;
 import java.util.*;
 
 public class Valid_Parentheses {
-    public static void main(String[] args) {
-
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if(stack.isEmpty()){
+                stack.push(s.charAt(i));
+            }else {
+                if(stack.peek()=='('&&s.charAt(i)==')'){
+                    stack.pop();
+                }else if(stack.pop()=='['&&s.charAt(i)==']'){
+                    stack.pop();
+                }else if(stack.pop()=='{'&&s.charAt(i)=='}'){
+                    stack.pop();
+                }
+            }
+        }
+        if(stack.isEmpty()){
+            return true;
+        }
+        return false;
     }
 }
