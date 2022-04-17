46f68bc2004a26ff620bfb0b74bdad8bbe3e441d
tph01198@naver.com <Stph01198@naver.com6>
2022-04-18 06:27:02 +0900

    :white_check_mark: leetcode_ReverseString

---
+++ src/main/kotlin/BOJ1000.kt
@@ -0,0 +1,15 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+class BOJ1000{
+}
+
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val st = StringTokenizer(br.readLine());
+    val a = st.nextToken().toInt()
+    val b  =st.nextToken().toInt()
+    val answer = a + b
+    println(answer)
+}
---
+++ src/main/kotlin/ReverseString.kt
@@ -0,0 +1,14 @@
+class ReverseString {
+    fun reverseString(s: CharArray): Unit {
+        var start = 0
+        var end = s.size -1
+        while(start<end){
+            var temp = s[start]
+            s[start] = s[end]
+            s[end] = temp
+            start++
+            end--
+        }
+    }
+}
+
