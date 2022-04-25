ec76371a1142a211e7da571c1d7069d60fe04e05
tph01198@naver.com <Stph01198@naver.com6>
2022-04-26 06:42:14 +0900

    :white_check_mark: programmers Lv1

--- src/main/kotlin/programmers/수박수박수박수박수박수.kt
+++ src/main/kotlin/programmers/수박수박수박수박수박수.kt
@@ -3,8 +3,8 @@ package programmers
 class 수박수박수박수박수박수 {
     fun solution(n: Int): String {
         var answer =""
-        for(i:Int in 1 until n){
-            when(n%2){
+        for(i:Int in 0 until n){
+            when(i%2){
                 0 -> answer+="수"
                 else -> answer +="박"
             }
