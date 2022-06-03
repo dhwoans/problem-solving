b43cf8d16469643117e9a5b8b59810596e642759
decoy <tph01198@naver.com>
2022-06-04 00:31:48 +0900

    2022-06-03

---
+++ algo/src/Leetcode/easy/MinCostClimbingStairs.java
@@ -0,0 +1,13 @@
+package Leetcode.easy;
+
+public class MinCostClimbingStairs {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length];
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for (int i = 2; i < dp.length; i++) {
+            dp[i]=Math.min(dp[i-1]+cost[i],dp[i-2]+cost[i]);
+        }
+        return Math.min(dp[dp.length-1],dp[dp.length-2]);
+    }
+}
---
+++ algo/src/programmers/Lv3/가장먼노드.java
@@ -0,0 +1,9 @@
+package programmers.Lv3;
+
+public class 가장먼노드 {
+    public int solution(int n, int[][] edge) {
+        int answer = 0;
+        //
+        return answer;
+    }
+}
