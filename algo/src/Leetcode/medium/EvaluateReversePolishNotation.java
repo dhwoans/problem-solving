c581fe3962d19200ff6d31b7b2264ca4997d3072
decoy <tph01198@naver.com>
2022-06-03 19:45:26 +0900

    2022-06-03

---
+++ algo/src/Leetcode/medium/EvaluateReversePolishNotation.java
@@ -0,0 +1,30 @@
+package Leetcode.medium;
+
+import java.util.Stack;
+
+public class EvaluateReversePolishNotation {
+
+    private Stack<Integer> stack;
+
+    public int evalRPN(String[] tokens) {
+        stack = new Stack<>();
+        for (String token : tokens) {
+            if(token.equals("*")||token.equals("+")||token.equals("/")||token.equals("-")){
+                cal(token);
+            }else{
+                stack.add(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+
+    private void cal(String token) {
+        int num1 = stack.pop();
+        int num2 = stack.pop();
+        if(token.equals("*"))stack.add(num1*num2);
+        else if(token.equals("+")) stack.add(num1+num2);
+        else if(token.equals("/")) stack.add(num2/num1);
+        else if(token.equals("-")) stack.add(num2-num1);
+        }
+    }
+}
