5f25bface3a117da3e059fbda79ce20142efd678
decoy <tph01198@naver.com>
2022-06-04 23:00:19 +0900

    2022-06-04

--- algo/src/BOJ/BOJ15664.java
+++
@@ -1,9 +0,0 @@
-package BOJ;
-
-public class BOJ15664 {//풀이중
-    static int n,r;
-    static int arr[];
-    public static void main(String[] args) {
-
-    }
-}
--- algo/src/BOJ/BOJ2477.java
+++
@@ -1,9 +0,0 @@
-package BOJ;
-
-
-import java.io.IOException;
-
-public class BOJ2477 {
-    public static void main(String[] args) throws IOException {
-    }
-}
