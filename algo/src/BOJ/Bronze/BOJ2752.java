ccf9ceff16a997811e2bc0a97878a1f7118867bd
decoy <tph01198@naver.com>
2022-06-11 14:44:38 +0900

    2022-06-10

--- algo/src/BOJ/Bronze/BOJ2752.java
+++ algo/src/BOJ/Bronze/BOJ2752.java
@@ -1,18 +1,19 @@
-package BOJ.Silver;
+package BOJ.Bronze;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class BOJ1629 {
+public class BOJ2752 {
     public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-
-
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            arr.add(Integer.parseInt(st.nextToken()));
+        }
+        arr.stream().sorted().forEach(i -> System.out.print(i+" "));
     }
 }
