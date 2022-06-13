7199639d1e2226873fefeb55512b9e40c3ce6ec3
decoy <tph01198@naver.com>
2022-06-13 14:04:55 +0900

    2022-06-13

--- algo/src/BOJ/Silver/BOJ11660.java
+++ algo/src/BOJ/Silver/BOJ11660.java
@@ -1,7 +1,6 @@
 package BOJ.Silver;
 
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,49 +8,42 @@ import java.util.StringTokenizer;
 
 public class BOJ11660 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        //값 입력
-        int sum =0;
-        int[] map = new int[N*N];
-        for (int i = 0; i < N; i++) {
-            st= new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                map[i*N+j]= Integer.parseInt(st.nextToken());
-                sum+=map[i*N+j];
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] map = new int[n][n];
+
+        for (int i = 0; i < map.length; i++) {
+            int sum = 0;
             st = new StringTokenizer(br.readLine());
-            int startX = Integer.parseInt(st.nextToken())-1;
-            int startY = Integer.parseInt(st.nextToken())-1;
-            int endX = Integer.parseInt(st.nextToken())-1;
-            int endY = Integer.parseInt(st.nextToken())-1;
-
-            int start=N*startX+startY;
-            int end=N*endX+endY;
-
-            int len = end - start;
-            int answer=0;
-            if(len<=map.length-len){
-                for (int j = start; j <= end; j++) {
-                    answer +=map[j];
-                }
-            }else{
-                for (int j = 0; j < start; j++) {
-                    answer +=map[j];
-                }
-                for (int j = end+1; j <map.length ; j++) {
-                    answer +=map[j];
-                }
-                answer=sum-answer;
+            for (int j = 0; j < map[i].length; j++) {
+                sum += Integer.parseInt(st.nextToken());
+                map[i][j] = sum;
             }
-            sb.append(answer+"\n");
-
         }
-        System.out.println(sb);
+
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken()) - 1;
+            int y1 = Integer.parseInt(st.nextToken()) - 1;
+            int x2 = Integer.parseInt(st.nextToken()) - 1;
+            int y2 = Integer.parseInt(st.nextToken()) - 1;
+
+            int answer = 0;
+            for (int j = x1; j <= x2; j++) {
+                if (y1 == 0) {
+                    answer += map[j][y2];
+                } else {
+                    answer += map[j][y2] - map[j][y1 - 1];
+                }
+            }
+            sb.append(answer).append("\n");
+        }
+        System.out.print(sb);
+        sb.setLength(0);
     }
 }
