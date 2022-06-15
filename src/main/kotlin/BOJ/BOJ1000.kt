d4894912831ebdf3a7910d6ba57fa42cb58cd8b5
decoy <tph01198@naver.com>
2022-06-15 23:23:13 +0900

    :white_check_mark:BOJ 최소 최대

--- src/main/kotlin/BOJ/BOJ1000.kt
+++ src/main/kotlin/BOJ/BOJ1000.kt
@@ -4,10 +4,8 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.StringTokenizer
 
-class BOJ1000{
-}
 
-fun main(){
+fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val st = StringTokenizer(br.readLine());
     val a = st.nextToken().toInt()
--- src/main/kotlin/BOJ/BOJ2562.kt
+++ src/main/kotlin/BOJ/BOJ2562.kt
@@ -4,6 +4,7 @@ import java.io.BufferedReader
 
 fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
     val arr = IntArray(9)
+
     for (i in 0 until arr.size) {
         arr[i] = readLine()!!.toInt()
     }
