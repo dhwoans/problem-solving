5f7eb386e2a5345dad53036385febefbff47b5dd
tph01198@naver.com <Stph01198@naver.com6>
2022-06-22 20:55:15 +0900

    :white_check_mark: 프로그래머스 최대공약수와 최소공배수

--- src/main/kotlin/BOJ/bronze/BOJ2985.kt
+++ src/main/kotlin/BOJ/bronze/BOJ2985.kt
@@ -1,5 +1,15 @@
 package BOJ.bronze
 
-fun main() {
+import java.io.BufferedReader
 
+fun main() =with(BufferedReader(System.`in`.bufferedReader())){
+    val (a,b,c) = readLine().split(" ").map { it.toInt() }
+    if(a+b == c) println("$a+$b=$c")
+    else if(a-b==c) println("$a-$b=$c")
+    else if(a/b==c) println("$a/$b=$c")
+    else if(a*b==c) println("$a*$b=$c")
+    else if(a==b+c) println("$a=$b+$c")
+    else if(a==b-c) println("$a=$b-$c")
+    else if(a==b/c) println("$a=$b/$c")
+    else if(a==b*c) println("$a=$b*$c")
 }
