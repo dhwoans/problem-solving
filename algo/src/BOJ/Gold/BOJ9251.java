03388c6490a2b3b96ecd966c7734db7f7e648724
decoy <tph01198@naver.com>
2022-06-24 19:50:30 +0900

    2022-06-24

---
+++ algo/src/BOJ/Gold/BOJ9251.java
@@ -0,0 +1,26 @@
+package BOJ.Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ9251 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+        int[][] lcs = new int[a.length()+1][b.length()+1];
+
+        for (int i = 1; i < lcs.length; i++) {
+            for (int j = 1; j < lcs[i].length; j++) {
+                if(a.charAt(i-1)==b.charAt(j-1)){
+                    lcs[i][j]= lcs[i-1][j-1]+1;
+                }else{
+                    lcs[i][j]=Math.max(lcs[i-1][j],lcs[i][j-1]);
+                }
+            }
+        }
+        System.out.println(Arrays.stream(lcs).flatMapToInt(Arrays::stream).max().getAsInt());
+    }
+}
