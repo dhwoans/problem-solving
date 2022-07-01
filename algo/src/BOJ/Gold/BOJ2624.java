40b88f8075a9a7f74665f993863ab39e0a50b109
decoy <tph01198@naver.com>
2022-07-01 19:40:18 +0900

    2022-07-01

--- algo/src/BOJ/Gold/BOJ2624.java
+++ algo/src/BOJ/Gold/BOJ2624.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class BOJ2624 {
     public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int t = Integer.parseInt(br.readLine());
         int k = Integer.parseInt(br.readLine());
         ArrayList<int[]> arr = new ArrayList<>();
