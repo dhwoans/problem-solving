b5432f5abd864469ef64e6965d7bdc4134a1363d
decoy <tph01198@naver.com>
2022-07-25 04:37:44 +0900

    :white_check_mark:BOJ 스타트링크

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,26 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val (n,m) = readLine()!!.split(" ").map { it.toInt() }
+    val arr = readLine()!!.split(" ").map { it.toInt() }.toIntArray()
+    val order = LongArray(n)
+    val reverseOrder =  LongArray(n)
+    var sum1:Long =0
+    var sum2:Long =0
+    for (i in 0 until order.size-1){
+        sum1+= if(arr[i]>arr[i+1])arr[i] - arr[i+1] else 0
+        sum2+= if(arr[arr.size-1-i]>arr[arr.size-2-i]) arr[arr.size-1-i] - arr[arr.size-2-i] else 0
+        order[i+1]= sum1
+        reverseOrder[i+1] = sum2
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    }
+
+    val sb = StringBuilder()
+    repeat(m){
+        val (from,to) = readLine()!!.split(" ").map { it.toInt()-1 }
+        if(from<to){
+            sb.append(order[to]-order[from]).append("\n")
+        }else{
+            sb.append(reverseOrder[n-1-to]-reverseOrder[n-1-from]).append("\n")
+        }
+    }
+    println(sb)
 }
