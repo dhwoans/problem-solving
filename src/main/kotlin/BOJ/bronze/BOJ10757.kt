efc8ec9c45536338532ee0497b3884a25a5e36dc
decoy <tph01198@naver.com>
2022-08-12 20:26:11 +0900

    :fire:

--- src/main/kotlin/BOJ/bronze/BOJ10757.kt
+++
@@ -1,8 +0,0 @@
-package BOJ.silver
-
-fun main() {
-    val br = System.`in`.bufferedReader()
-    val (n,m) = br.readLine().split(" ").map{it.toBigDecimal()}
-    println(n.plus(m))
-
-}
--- src/main/kotlin/BOJ/silver/BOJ11656.kt
+++
@@ -1,9 +0,0 @@
-package BOJ.silver
-
-import java.io.BufferedReader
-
-fun main()=with(BufferedReader(System.`in`.bufferedReader())) {
-    val s = readLine()
-    val arr =Array(s.length) { s.substring(it, s.length) }.sortedArray().forEach { println(it)}
-
-}
