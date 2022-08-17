95f95f15b34a718e8981bf909f4a90f6de14509f
decoy <tph01198@naver.com>
2022-08-17 19:20:29 +0900

    :white_check_mark: BOJ 성곽

---
+++ src/main/kotlin/BOJ/bronze/BOJ2338.kt
@@ -0,0 +1,9 @@
+package BOJ.bronze
+
+fun main() {
+    val num1 = readLine()!!.toBigInteger()
+    val num2 = readLine()!!.toBigInteger()
+    println(num1+num2)
+    println(num1-num2)
+    println(num1*num2)
+}
