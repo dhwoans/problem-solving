c6f530be6c97b709e79bdfcc4350ebd808acce5e
decoy <tph01198@naver.com>
2022-08-19 22:42:06 +0900

    :white_check_mark: BOJ 여왕벌

--- src/main/kotlin/BOJ/bronze/BOJ3003.kt
+++
@@ -1,9 +0,0 @@
-package BOJ.bronze
-
-fun main() {
-    val arr = intArrayOf(1,1,2,2,2,8)
-    val sb = StringBuilder()
-    val chess = readLine()!!.split(" ").map(String::toInt).toIntArray().forEachIndexed{ index, i ->
-        print("${arr[index]-i} ")
-    }
-}
