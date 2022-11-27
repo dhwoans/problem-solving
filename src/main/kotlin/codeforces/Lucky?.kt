1c3f69ced87ff4ef3ec26374365c9defc0b248d5
OHJAEMOON <51963264+DECOY-DUCK@users.noreply.github.com>
2022-11-27 23:14:53 +0900

    Delete Lucky?.kt

--- src/main/kotlin/codeforces/Lucky?.kt
+++
@@ -1,9 +0,0 @@
-package codeforces
-
-
-fun main() {
-    repeat(readLine()!!.toInt()) {
-        val str = readLine()!!.toCharArray().map{it-'0'}.toIntArray()
-        println(if ((str[0] + str[1] + str[2]) == str[3] + str[4] + str[5]) "YES" else "NO")
-    }
-}
